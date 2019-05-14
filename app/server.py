91c2bc7da395069b87033d4689fa89735d83b89e
Dakini <davidgillespie1987@gmail.com>
2019-05-14 14:09:44 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/c2jbdqd6flm57ey/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/c2jbdqd6flm57ey/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
